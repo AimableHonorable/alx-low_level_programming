6c4fcb2bae97f52b675148088db55e2be2f4ea69
AimableHonorable <aimatwiri@gmail.com>
2021-06-18 23:24:10 +0200

    correction

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,21 +12,20 @@ for (i = 48; i < 57; i++)
 {
 for (j = 48; j < 58; j++)
 {
-        for (k = 48; k < 58; k++)
-        {
-                if (j > i && k >j)
-                {
-                putchar(i);
-                putchar(j);
-                putchar(k);
-                if (i != 55 || j != 56 || k != 57)
-                {
-                putchar(',');
-                putchar(' ');
-                }
-                }
-        }
-
+for (k = 48; k < 58; k++)
+{
+if (j > i && k >j)
+{
+putchar(i);
+putchar(j);
+putchar(k);
+if (i != 55 || j != 56 || k != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
 }
 }
 putchar('\n');
