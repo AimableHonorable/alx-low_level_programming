02e4e33280872cda838ef9ab1f267dd4bcaca290
AimableHonorable <aimatwiri@gmail.com>
2021-06-18 12:14:14 +0200

    making clea codes

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,19 +5,23 @@
 /**
  * main - function to tell whether number
  * is negative or positive or zero
- * Return: must return zero when successful */
+ * Return: must return zero when successful 
+*/
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0) {
+	if (n < 0) 
+	{
 		printf ("%d is negative\n", n);
-	} else if (n == 0) {
+	} else if (n == 0) 
+	{
 		printf("%d is zero\n", n);
-	} else {
-		printf ("%d is positive\n", n);
+	} else 
+	{
+		printf("%d is positive\n", n);
 	}
 	return (0);
 }
