ae5c024f7252dc5978036debe06eb29196b6facb
AimableHonorable <aimatwiri@gmail.com>
2021-06-18 22:04:50 +0200

    print hexadecimabl numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - prints all the numbers of base 16 in lowercase,
+* followed by a new line
+* Return: returns 0 when successful
+*/
+int main(void)
+{
+int n;
+char ch;
+
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+for (ch = 'A'; ch <= 'F'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
