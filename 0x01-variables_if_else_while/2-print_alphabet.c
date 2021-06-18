024bea43c2ea1a6a170af162f8e1b0ac7d7244c1
AimableHonorable <aimatwiri@gmail.com>
2021-06-18 13:46:56 +0200

    print alphatets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,10 +3,13 @@
 * main - Print all aphabets in small letters
 * Return: should return zero when successful
 */
-int main(void) {
+int main(void)
+{
   char c;
   for (c = 'a'; c <= 'z'; ++c)
+  {
     putchar(c);
+  }
   putchar('\n');
   return 0;
 }
