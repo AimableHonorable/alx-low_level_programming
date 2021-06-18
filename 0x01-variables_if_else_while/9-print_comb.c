10c8c0f1866987c5b83c274ccbdde75d2a6c5bd1
AimableHonorable <aimatwiri@gmail.com>
2021-06-18 22:47:18 +0200

    print numbers separated by comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Print all decimals less than 10
+* startingfrom zero and use putchar
+* Return: should return zero when successful
+*/
+int main(void)
+{
+int i;
+
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+if (i != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
