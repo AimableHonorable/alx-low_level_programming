8b07940c5b19e44527a0e5137ee25586db7dee0e
AimableHonorable <aimatwiri@gmail.com>
2021-06-22 23:47:05 +0200

    print sequence of natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,23 +6,25 @@
 
 void print_to_98(int n)
 {
-	if(n <=98)
-	{
-		while (n <= 98)
-		{
-			printf("%d, ", n);
-			n++;
-		}
-		printf("\n");
-	}
+	int i, j;
 
-	if (n >= 98)
+	if (n <= 98)
 	{
-		while (n >= 98)
-        	{
-	                printf("%d, ", n);
-                	n--;
-        	}
-        	printf("\n");
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
 	}
 }
