da6d8db44338e1d38e7a7106ba811c8bb487e6df
AimableHonorable <aimatwiri@gmail.com>
2021-06-23 08:40:35 +0200

    multiplication table up to 15

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+
+/**
+ * print_times_table - To print multiplication table of numbers
+ * between 0 and 15
+ * @n: a given number between 0 and 15
+ */
+
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	if (n<= 15 && n>= 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = i * j;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				}
+
+				if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
