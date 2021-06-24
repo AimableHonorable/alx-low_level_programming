89e467e91b6f00fa0a8fddcae2bb55694ea9a47f
AimableHonorable <aimatwiri@gmail.com>
2021-06-24 12:22:56 +0200

    correction

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,12 +24,12 @@ printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-  if ((month == 2 && day == 60) && (year % 4 == 0))
+if ((month == 2 && day == 60) && (year % 4 == 0))
 {
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
- else if ((month == 2 && day == 60) && (year % 4 != 0))
+else if ((month == 2 && day == 60) && (year % 4 != 0))
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
