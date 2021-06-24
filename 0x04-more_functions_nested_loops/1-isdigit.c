df25578308d152f25ca5627b1ea9f743a43be32a
AimableHonorable <aimatwiri@gmail.com>
2021-06-24 18:34:48 +0200

    check for digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - function to check digit from 0 to 9
+ * @c: int to be checked
+ * Return: 1 if it is a digit and 0 if not
+ */
+int _isdigit(int c)
+{
+	return (c >= 0 && c <= 9);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 ## 0x04. C - More functions, more nested loops
 * Function to check for uppercase character
+* Function to check for digits
