b2080214d9c6a63f2db0e5e4786c76567c81ef3b
AimableHonorable <aimatwiri@gmail.com>
2021-06-24 18:49:32 +0200

    multiply two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * mul - a function to multiply two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: should return 0 when true
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 ## 0x04. C - More functions, more nested loops
 * Function to check for uppercase character
 * Function to check for digits
+* Function to multiply two integers
