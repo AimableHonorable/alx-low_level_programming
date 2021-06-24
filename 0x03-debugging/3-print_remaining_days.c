f25e879088b48aa5991e5e505edbae1f2c257fb9
AimableHonorable <aimatwiri@gmail.com>
2021-06-24 11:56:49 +0200

    debugging function to print remaining days of a year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,7 +24,7 @@ printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-if ((month == 2 && day == 60) && ((year % 4 == 0 || year % 400 == 0)))
+if ((month == 2 && day == 60) && (year % 4 == 0 || year % 400 == 0))
 {
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
